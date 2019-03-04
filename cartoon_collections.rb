50665bfae24af3a0000a0652ae317351b4025930
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 05:34:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
   dwarves.each_with_index do |dwarf, index|
     puts "#{index +1}. #{dwarf}"# code an argument here
 end  # Your code here
