9ae04fc609728f602c757be4a11b8a37168db218
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 07:11:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,10 +20,11 @@ end
 end
 
 
-def find_the_cheese(cheese_types)# code an argument here
+def find_the_cheese(cheese_types)
+  cheese_types = ["cheddar", "gouda", "camembert"]# code an argument here
   cheese_types.find do |word|
     return word
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
 end
